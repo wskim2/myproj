652ce005fb472e3497c73f6bab42af6381589691
wskim <wskim5601@gmail.com>
2015-12-02 11:43:52 -0800

    push init proj

---
+++ endian test/endian test/endian.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void endian_test1()
+{
+union{
+	unsigned int num;
+	unsigned char bytes[sizeof(unsigned int)];
+	}u;
+int i;
+
+	u.num = 0x0a0b0c0d;
+
+	for(i=0;i<sizeof(unsigned int);i++)
+		printf("%x: %.2x\n", &u.bytes[i], u.bytes[i]);
+}
+
+void endian_test2()
+{
+	unsigned int num=1;
+	
+	if(*(char*)&num == 1)
+		printf("\nLittel Endian\n");
+	else
+		printf("\nBig Endian\n");
+
+}
+
+int endian_conversion(unsigned char endian, unsigned int num)
+{
+unsigned int b0,b1,b2,b3;
+unsigned int res, swapped;
+
+b0 = (num & 0x000000ff) << 24u;
+b1 = (num & 0x0000ff00) << 8u;
+b2 = (num & 0x00ff0000) >> 8u;
+b3 = (num & 0xff000000) >> 24u;
+
+res = b0 | b1 | b2 | b3;
+printf("converted endian from 0x%x to 0x%x\n", num, res);
+
+//2nd swap number
+swapped =((num>>24)&0xff) | // move byte 3 to byte 0
+         ((num<<8)&0xff0000) | // move byte 1 to byte 2
+         ((num>>8)&0xff00) | // move byte 2 to byte 1
+         ((num<<24)&0xff000000);// byte 0 to byte 3
+
+printf("converted endian from 0x%x to 0x%x\n", num, res);
+}
+	
+void main()
+{
+	 endian_test2();
+	 endian_conversion(1, 0x12345678);
+
+	 getch();
+}
---
+++ linked list/linked_list.c
@@ -0,0 +1,259 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node{
+	int data;
+	struct Node* next;
+}Node;
+
+Node *head = NULL;
+
+void Print()
+{
+	Node *temp = head;
+
+	printf("List is : ");
+	while(temp != NULL)
+	{
+		printf(" %d", temp->data);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+void Insert(Node** head, int x)
+{
+	Node* temp = (Node *) malloc(sizeof(Node));
+	temp->data = x;
+	temp->next = NULL;
+	if(*head != NULL) 
+		temp->next= *head;
+	*head = temp;
+}
+
+//insert new node to pos from head.
+void Insert_pos(int x, int pos)
+{
+	Node* temp2;
+	int i;
+	Node* temp1 = (Node *) malloc(sizeof(Node));
+
+	temp1->data = x;
+	temp1->next = NULL;
+
+	if(pos ==1)
+	{
+		temp1->next = head;
+		head = temp1;
+		return;
+	}
+
+	temp2 = head;
+	for(i=0;i<pos-2;i++)
+	{
+		temp2 = temp2->next;
+	}
+
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+
+}
+
+void Delete(int n)
+{
+	int i;
+	Node *temp2;
+	Node *temp1 = head;
+
+	if(n == 1) //1st node right after head
+	{
+		head = temp1->next;
+		free(temp1);
+		return;
+	}
+
+	for(i=0;i<n-2;i++)
+		temp1 = temp1->next;
+	
+	temp2 = temp1->next;
+	temp1->next = temp2->next;
+	free(temp2);
+}
+
+// to reverse inked list iterative method : converting head node to tail node.
+void Reverse()
+{
+	Node *current, *prev, *next;
+
+	current = head;
+	prev = NULL;
+	while(current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	head = prev;
+
+	printf("Reverse List ->");
+	Print();
+}
+
+//test recursive call in printing node
+void ReversePrint(Node *p)
+{
+	if(p == NULL) //tail
+	{
+		return;
+	}
+
+	//printf("%d ", p->data);
+	ReversePrint(p->next);
+	printf("%d ", p->data);
+}
+
+
+//reverse linked list with recursion
+void Reverse_recursive(Node *p)
+{
+	if(p->next == NULL)
+	{
+		head = p;
+		return;
+	}
+	Reverse_recursive(p->next);
+	p->next->next = p;
+	p->next = NULL;	
+}
+
+
+// insert new node from head node
+void test1()
+{
+	int n,i,x;
+
+	printf("how may numbers?\n");
+	
+	scanf("%d", &n);
+
+	for(i=0;i<n;i++)
+	{ 
+		printf("Enter num?\n");
+		scanf("%d",&x);
+		Insert(&head, x);
+		Print(head);
+	}
+}
+
+void test2()
+{
+	Insert_pos(2,1);
+	Insert_pos(3,2);
+	Insert_pos(4,1);
+	Insert_pos(5,2);
+	Print();
+}
+
+void test3()
+{
+	int n,i;
+
+	for(i=0;i<4;i++)
+	{
+		printf("Enter delete position?");
+		scanf("%d", &n);
+		Delete(n);
+		Print();
+	}
+}
+
+//recursive call 1
+int Fibo(int n)
+{
+int sum;
+
+if (n <= 1) 
+	return n;
+
+
+sum = Fibo(n-1) + Fibo(n-2);
+
+return sum;
+}
+
+//recursive call with memoization to save time
+int F[51];
+int Fibo2(int n)
+{
+
+if(F[n] != -1) //if we already calculated or know the value
+	return F[n];
+
+F[n] = Fibo2(n-1) + Fibo2(n-2);
+
+return F[n];
+}
+ 
+//0 1 1 2 3 5 8 13 21 ....
+void calc_fibonachi(void)
+
+{
+int i, n, num;
+
+	printf(" enter n for fibonachi? ");
+	scanf("%d", &n);
+
+	for(i=0;i<51;i++)
+		F[i] = -1;
+	
+	F[0]=0; 
+	F[1] =1;
+
+	//num=Fibo2(n);
+	num = Fibo(n);
+	printf("Fibo of %d is %d \n ", n, num);
+
+}
+
+
+
+
+//recursive call 2
+int factorial(int n)
+{
+	int mul;
+
+	if(n==1) 
+		return 1;
+	else
+		mul = factorial(n-1)*n;
+
+	return mul;
+
+}
+void sim_factorial()
+{
+	int n, res;
+
+	printf(" enter n for factorial? ");
+	scanf("%d", &n);
+
+	res= factorial(n);
+	printf("\n result of fact(%d) = %d\n", n, res);
+}
+
+int main()
+{
+		
+	//test1();
+	test2();
+	//test3();
+	//Reverse();
+	//ReversePrint(head);
+	//Reverse_recursive(head);
+	//Print();
+	calc_fibonachi();
+	//sim_factorial();
+
+	getch();
+}
